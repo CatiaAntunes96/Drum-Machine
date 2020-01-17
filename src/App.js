a41672b806edcdf5ce412b176e32e3946684b504
Catia Antunes <klisar1996@gmail.com>
2020-01-17 23:12:28 +0000

    Add all User Storys
    Finish handle key press event

--- src/App.js
+++ src/App.js
@@ -2,21 +2,12 @@ import React from 'react';
 
 import DrumPad from "./Drum-Pad";
 import './App.css';
-
-
  
 function App() {
-  const playSound = () => {
-    const audioElement = document.getElementsByClassName("clip")[5];
-    audioElement.play();
-  } 
-
-
+ 
   return (
     <div id="drum-machine">
-      <section id="display">
       <DrumPad /> 
-      </section>
     </div>
   );
 }
--- src/Drum-Pad.js
+++ src/Drum-Pad.js
@@ -1,85 +1,30 @@
 import React from "react";
 import Drum from "./Drum";
 
-function DrumPads() {
+function DrumPad() {
 
- const drumsArray = [{soundDescription:"heavy_kick", innerText:"Q", soundSrc:"heavy-kick.wav"},
- {soundDescription:"tom", innerText:"W", soundSrc:"tom.wav"},
- {soundDescription:"piano_key", innerText:"E", soundSrc:"piano_key.wav"},
-//  {soundDescription:"", innerText:"", soundSrc:""},
-//  {soundDescription:"", innerText:"", soundSrc:""},
-//  {soundDescription:"", innerText:"", soundSrc:""},
-//  {soundDescription:"", innerText:"", soundSrc:""},
-//  {soundDescription:"", innerText:"", soundSrc:""},
-//  {soundDescription:"", innerText:"", soundSrc:""}
+ const drumsArray = [{soundDescription:"Heavy Kick", innerText:"Q", soundSrc:"heavy_kick.wav"},
+ {soundDescription:"Tom", innerText:"W", soundSrc:"tom.wav"},
+ {soundDescription:"Piano Key", innerText:"E", soundSrc:"piano_key.wav"},
+ {soundDescription:"Hat", innerText:"A", soundSrc:"hat.wav"},
+ {soundDescription:"Snr One", innerText:"S", soundSrc:"snr_one.wav"},
+ {soundDescription:"Clap One", innerText:"D", soundSrc:"clap_one.wav"},
+ {soundDescription:"Clap Two", innerText:"Z", soundSrc:"clap_two.wav"},
+ {soundDescription:"Cymbal", innerText:"X", soundSrc:"cymbal.wav"},
+ {soundDescription:"Snr two", innerText:"C", soundSrc:"snr_two.wav"}
 ]
-
 
     return (
         <div>
+        <h4 id="display">{drumsArray.soundDescription}</h4>
+        <div className="container">
             {
-                drumsArray.map(d => (<Drum key = {d.soundDescription}  {...d}/>) )
+                drumsArray.map(d => (<Drum key = {d.soundDescription}  {...d} />) )
             }
-{/* 
-<div className="drum-pad" id="heavy_kick" onClick={playSound}>
-                <p>Q</p> 
-                <audio className="clip" id="Q" >
-                <source src="heavy-kick.wav" type="audio/wav" />
-                </audio>
-            </div>
-            <div className="drum-pad" id="tom">
-                <p>W</p>
-                <audio className="clip" id="W">
-                <source src="tom.wav" type="audio/wav" />
-                </audio>
-            </div>
-            <div className="drum-pad" id="piano_key">
-                <p>E</p>
-                <audio className="clip" id="E">
-                <source src="piano_key.wav" type="audio/wav" />
-                </audio>
-            </div>
-            <div className="drum-pad" id="hat">
-                <p>A</p>
-                <audio className="clip" id="A">
-                <source src="hat.wav" type="audio/wav" />
-                </audio>
-            </div>
-        </div>
-        <div className="second-row">
-            <div className="drum-pad" id="snr_one">
-                <p>S</p>
-                <audio className="clip" id="S">
-                <source src="snr_one.wav" type="audio/wav" />
-                </audio>
-            </div>
-            <div className="drum-pad" id="clap_one" onClick={playSound}>
-                <p>D</p>
-                <audio className="clip" id="D">
-                <source src="clap_one.wav" type="audio/wav" />
-                </audio>
-            </div>
-            <div className="drum-pad" id="clap_two">
-                <p>Z</p>
-                <audio className="clip" id="Z">
-                <source src="clap_two.wav" type="audio/wav" />
-                </audio>
-            </div>
-            <div className="drum-pad" id="cymbal">
-                <p>X</p> 
-                <audio className="clip" id="X">
-                <source src="cymbal.wav" type="audio/wav" />
-                </audio>
-            </div>
-            <div className="drum-pad" id="snr_two">
-                <p>C</p>
-                <audio className="clip" id="C">
-                <source src="snr_two" type="audio/wav" />
-                </audio>
-            </div> */}
 
+        </div>
         </div>
         );
     }
 
-export default DrumPads;
+export default DrumPad;
